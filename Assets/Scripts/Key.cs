fe67a8feae0e28729e99204ae2c8c37d03b8d543
Dan Tulovsky <dant@lightstep.com>
2021-12-18 23:07:19 -0500

    key locks

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+
+    [SerializeField] private KeyLock keyLock;
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
         
         Player player = col.GetComponent<Player>();
-        if (player == null) return;
+        if (player != null)
+        {
+            transform.SetParent(player.transform);
+            transform.localPosition = Vector3.up;
+        }
         
-        transform.SetParent(player.transform);
-        transform.localPosition = Vector3.up;
+        KeyLock keyLock = col.GetComponent<KeyLock>();
+        if (keyLock != null && keyLock == this.keyLock)
+        {
+            keyLock.Unlock();
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/KeyLock.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyLock : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onUnlocked;
+
+    public void Unlock()
+    {
+        onUnlocked.Invoke();
+    }
+}
