b5fd48accf9a9dd59e1c5ed72bf705f31fd29d1c
Dan Tulovsky <dant@lightstep.com>
2021-12-19 10:57:58 -0500

    particle collision

--- Assets/Scripts/KillOnEnter.cs
+++ Assets/Scripts/KillOnEnter.cs
@@ -1,10 +1,20 @@
+using System;
 using UnityEngine;
 
 public class KillOnEnter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Player player = col.GetComponent<Player>();
+        ResetPlayer(col.GetComponent<Player>());
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ResetPlayer(other.GetComponent<Player>());
+    }
+
+    private void ResetPlayer(Player player)
+    {
         if (player == null) return;
 
         player.ResetToStart();
