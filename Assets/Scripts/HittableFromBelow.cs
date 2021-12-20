a473e43316f001c84b183629176e44f929956c7c
Dan Tulovsky <dant@lightstep.com>
2021-12-19 22:30:38 -0500

    inheritance

--- Assets/Scripts/HittableFromBelow.cs
+++ Assets/Scripts/HittableFromBelow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class HittableFromBelow : MonoBehaviour
@@ -7,9 +8,17 @@ public class HittableFromBelow : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     protected virtual bool CanUse => true;
 
+    private Animator _animator;
+    private static readonly int UseTrigger = Animator.StringToHash("Use");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     protected virtual void Start()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -21,10 +30,17 @@ public class HittableFromBelow : MonoBehaviour
 
         // Hit from below
         if (!(col.GetContact(0).normal.y > 0)) return;
-        
+
+        PlayAnimation();
         Use();
         if (!CanUse)
             _spriteRenderer.sprite = usedSprite;
+    }
+
+    private void PlayAnimation()
+    {
+        if (_animator)
+            _animator.SetTrigger(UseTrigger);
     }
 
     protected virtual void Use()
