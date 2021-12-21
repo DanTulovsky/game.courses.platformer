7f493da53f5e04b261e2af44f1af974fc8c834ca
Dan Tulovsky <dant@lightstep.com>
2021-12-21 10:50:02 -0500

    player pref

---
+++ Assets/Scripts/UILockable.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UILockable : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var startButton = GetComponent<UIStartLevelButton>();
+
+        string key = startButton.LevelName + "Unlocked"; // e.g. "Level1Unlocked"
+        int unlocked = PlayerPrefs.GetInt(key, 0);
+
+        if (unlocked == 0)
+            gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/UIStartLevelButton.cs
+++ Assets/Scripts/UIStartLevelButton.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,13 +7,10 @@ public class UIStartLevelButton : MonoBehaviour
 {
     [SerializeField] private string levelName;
 
+    public string LevelName => levelName;
+
     public void LoadLevel()
     {
-        SceneManager.LoadScene(levelName);
-    }
-
-    private void OnValidate()
-    {
-        GetComponentInChildren<TMP_Text>()?.SetText(levelName);
+        SceneManager.LoadScene(LevelName);
     }
 }
